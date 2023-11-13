df9b8f1762fa4f983dc7744d9a7be30f09b0bff0
akakinozadze <145987347+akakinozadze@users.noreply.github.com>
2023-11-13 12:59:31 +0400

    header done

---
+++ js/form.js
@@ -0,0 +1,7 @@
+let form12 = document.getElementById("form1");
+let icon1 = document.getElementById("icon");
+
+icon1.addEventListener("click", function() {
+    form12.classList.toggle("newform");
+
+})
